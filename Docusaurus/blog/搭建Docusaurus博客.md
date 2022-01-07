375f41a8af26c747ee133dd82d4a0efc980213c7
gaizi <1424598066@qq.com>
2022-01-07 11:15:08 +0800

    add blog

---
+++ Docusaurus/blog/搭建Docusaurus博客.md
@@ -0,0 +1,8 @@
+---
+slug: welcome
+title: 搭建Docusaurus博客并部署在Github Pages上
+authors: gaizi
+tags: [Blog, docusaurus, Github Pages]
+---
+
+这是一个教程
--- Docusaurus/docusaurus.config.js
+++ Docusaurus/docusaurus.config.js
@@ -25,6 +25,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                     editUrl: 'https://github.com/gai-zi/Website/blob/main/Docusaurus/',
                 },
                 blog: {
+                    blogTitle: 'Docusaurus 博客！',
+                    blogDescription: '这是个用 Docusaurus 搭建的博客！',
+                    postsPerPage: 1,
                     showReadingTime: true,
                     // Please change this to your repo.
                     editUrl: 'https://github.com/gai-zi/Website/blob/main/Docusaurus/',
